5af1f476c7adc8797bedddddc4cc851e47e720b8
akemi-adam <bazilixo8@gmail.com>
2025-04-08 21:43:27 -0300

    feat: função que realiza a busca linear recursiva

--- search/search.c
+++ search/search.c
@@ -7,3 +7,16 @@ int i_linear_search(int* array, int length, int target)
         if (array[i] == target) return i;
     return -1;
 }
+
+int r_linear_search(int* array, int length, int target)
+{
+    length--;
+
+    if (length > NOT_FOUND_INDEX)
+    {
+        if (array[length] == target) return length;
+        return r_linear_search(array, length, target);
+    }
+
+    return NOT_FOUND_INDEX;
+}
