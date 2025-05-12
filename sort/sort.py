45c70230b21e466e8f91e5719597d9a3d2804130
akemi-adam <bazilixo8@gmail.com>
2025-05-12 10:20:10 -0300

    feat: implementação iterativa do insertion sort em python

--- sort/sort.py
+++ sort/sort.py
@@ -2,3 +2,11 @@ def swap(v: list, i: int, j: int) -> None:
     v[i], v[j] = v[j], v[i]
 
 
+def i_insertion_sort(v: list) -> None:
+    for i in range(1, len(v)):
+        j: int = i - 1
+        while j >= 0 and v[j] > v[j + 1]:
+            swap(v, j, j + 1)
+            j -= 1
+
+                
