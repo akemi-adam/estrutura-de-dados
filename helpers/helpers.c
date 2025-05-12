c0c7619f805ec5db4b177bdbdf229b6db735dc10
akemi-adam <bazilixo8@gmail.com>
2025-05-12 10:16:21 -0300

    feat: função merge

--- helpers/helpers.c
+++ helpers/helpers.c
@@ -15,3 +15,29 @@ void int_swap(int* v, int i, int j) {
     v[j] = temp;
 }
 
+/**
+ * Compara dois arrays, monta um array ordenado a partir dessa comparação e substitui no array original
+ * 
+ * @param int* v: Array de inteiros
+ * @param int s: Início do array
+ * @param int m: Meio do array
+ * @param int e: Final do array
+ */
+void merge(int* v, int s, int m, int e) {
+    int len = e - s + 1,
+        i = s,
+        j = m + 1,
+        w[len];
+    
+    for (unsigned int k = 0; k < len; k++) {
+        if ((j > e) || (i <= m && v[i] < v[j])) {
+            w[k] = v[i];
+            i++;
+        } else {
+            w[k] = v[j];
+            j++;
+        }
+    }
+
+    for (unsigned int k = 0; k < len; k++) v[s + k] = w[k];
+}
